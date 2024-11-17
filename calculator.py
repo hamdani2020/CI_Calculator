0e97540bdd18732e750cde4fbda7b4f2be285fa5
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-11-17 18:49:53 +0000

    Fixed division error

--- calculator.py
+++ calculator.py
@@ -13,3 +13,7 @@ def substract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+    return first_term / second_term
